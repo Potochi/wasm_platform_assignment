ec28654b4fa1a84e11f96e9b00b4d39748d47c12
Potochi <budliviu@gmail.com>
2023-04-28 18:18:39 +0300

    make migrator binary retry in case of errors

--- backend/src/bin/migrator.rs
+++ backend/src/bin/migrator.rs
@@ -1,16 +1,33 @@
-use aws_backend::{constants, migrator};
+use aws_backend::migrator;
 use sea_orm::{ConnectOptions, Database};
 use sea_orm_migration::MigratorTrait;
+use tokio::time::{sleep, Duration};
 
-#[tokio::main]
-async fn main() -> anyhow::Result<()> {
-    let mut db_opts = ConnectOptions::new(
-        std::env::var("DB_URL").unwrap_or(constants::DEFAULT_DB_URL.to_string()),
-    );
-    db_opts.sqlx_logging(false);
+async fn attempt_migrations() -> anyhow::Result<()> {
+    let mut db_opts = ConnectOptions::new(std::env::var("DB_URL").expect("DB_URL to be present"));
+    db_opts.sqlx_logging(true);
 
     let db = Database::connect(db_opts).await?;
     migrator::Migrator::up(&db, None).await?;
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::fmt::try_init()
+        .map_err(|_| anyhow::anyhow!("Failed to install tracing_subscriber"))?;
+
+    let sleep_secs = u64::from_str_radix(
+        &std::env::var("RETRY_SLEEP_SECS").unwrap_or("5".to_owned()),
+        10,
+    )
+    .map_err(|_| anyhow::anyhow!("Invalid sleep duration"))?;
+
+    while let Err(_) = attempt_migrations().await {
+        tracing::info!("Could not run migrations, retrying...");
+        sleep(Duration::from_secs(sleep_secs)).await;
+    }
 
     Ok(())
 }
