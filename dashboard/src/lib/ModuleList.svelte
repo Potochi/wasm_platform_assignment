090c8d765f5eb66977d0f71bdc55f8b3846cb0aa
Potochi <budliviu@gmail.com>
2023-05-03 03:53:34 +0300

    fix bug making a bajillion requests to backend

--- dashboard/src/lib/ModuleList.svelte
+++ dashboard/src/lib/ModuleList.svelte
@@ -7,8 +7,7 @@
 </style>
 
 <script lang="ts">
-import { onMount } from "svelte";
-import { ApiResponse, type ApiModuleType, apiUrl } from "../ApiTypes";
+import { ApiResponse, apiUrl } from "../ApiTypes";
 import { userJwt } from "../auth";
 import Module from "./Module.svelte";
 import { userModules } from "../modules";
@@ -25,16 +24,18 @@ const fetchModules = async () => {
   $userModules = ApiResponse.parse(data).modules;
 };
 
-$: if ($userModules.length == 0) {
+$: if ($userModules == undefined) {
   fetchModules().catch((e) => console.log(e));
 }
 </script>
 
 <div class="moduleList">
-  {#each $userModules as module}
-    <Module
-      name="{module.module_hash}"
-      functions="{module.functions}"
-      moduleId="{module.id}" />
-  {/each}
+  {#if $userModules != undefined}
+    {#each $userModules as module}
+      <Module
+        name="{module.module_hash}"
+        functions="{module.functions}"
+        moduleId="{module.id}" />
+    {/each}
+  {/if}
 </div>
--- dashboard/src/modules.ts
+++ dashboard/src/modules.ts
@@ -1,4 +1,4 @@
 import { writable } from "svelte/store";
 import type { ApiModuleType } from "./ApiTypes";
 
-export const userModules = writable<ApiModuleType[]>([]);
+export const userModules = writable<ApiModuleType[] | undefined>(undefined);
