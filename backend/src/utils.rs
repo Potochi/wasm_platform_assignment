41faa2d8370982ea123757985a600551693298c4
Potochi <budliviu@gmail.com>
2023-05-03 03:52:40 +0300

    cooler wasm cost function

--- backend/src/utils.rs
+++ backend/src/utils.rs
@@ -8,9 +8,20 @@ lazy_static! {
         Arc::new(wasmer_middlewares::Metering::new(10, wasm_cost_function));
 }
 
-// TODO(Livian): More sophisticated cost function
-pub fn wasm_cost_function(_: &Operator) -> u64 {
-    1
+pub fn wasm_cost_function(op: &Operator) -> u64 {
+    match op {
+        Operator::LocalGet { local_index: _ } => 1,
+        Operator::LocalSet { local_index: _ } => 2,
+        Operator::LocalTee { local_index: _ } => 3,
+        Operator::GlobalGet { global_index: _ } => 4,
+        Operator::GlobalSet { global_index: _ } => 5,
+        Operator::I32Load { memarg: _ } => 6,
+        Operator::I64Load { memarg: _ } => 7,
+        Operator::F32Load { memarg: _ } => 8,
+        Operator::F64Load { memarg: _ } => 9,
+        Operator::F32x4RelaxedDotBf16x8AddF32x4 => 100_000,
+        _ => 1,
+    }
 }
 
 pub fn password_secure_check(pass: &str) -> bool {
