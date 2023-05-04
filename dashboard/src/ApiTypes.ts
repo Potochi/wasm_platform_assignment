f3bb4d9b5f9bb58a2256ad6874336dd1085cd3d1
Potochi <budliviu@gmail.com>
2023-05-04 21:38:28 +0300

    move url scheme out of dashboard constant

--- dashboard/src/ApiTypes.ts
+++ dashboard/src/ApiTypes.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
-export const apiUrl = `http://${import.meta.env.VITE_BACKEND_URL}/api/v1`;
+export const apiUrl = `${import.meta.env.VITE_BACKEND_URL}/api/v1`;
 
 export const ApiFunction = z
   .object({
